49eea2a67303ab248c6cf187fd929d5f4ea66b47
Hugo Santos <hf.santos2332@gmail.com>
2023-07-12 17:39:28 -0300

    Adiciona leitura de variáveis de ambiente no projeto

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import {config} from 'dotenv';
 
 const app = express();
+config(); // Configura variáveis de ambiente (.env)
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.APP_PORT || 8080;
 
 app.listen(PORT, () => {
   console.log('Servidor iniciado na porta ' + PORT);

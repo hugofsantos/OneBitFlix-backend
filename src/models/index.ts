b6521dd1519513d737a63082569fc5837f834439
Hugo Santos <hf.santos2332@gmail.com>
2023-08-03 21:37:08 -0300

    Cria migration e models de episodes

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,15 @@
 import { categoryModel } from "./Category";
 import { courseModel } from "./Courses";
+import { episodeModel } from "./Episode";
 
 categoryModel.hasMany(courseModel); // Uma categoria tem muitos cursos
 courseModel.belongsTo(categoryModel); // Pertence à uma categoria
 
+courseModel.hasMany(episodeModel); // Um curso possui muitos episódios
+episodeModel.belongsTo(courseModel); // Um episódio pertence à um curso
+
 export {
   categoryModel,
-  courseModel
+  courseModel,
+  episodeModel
 };

5fcad6dc0a62e0a93187291bd1485042edf9e2a6
Hugo Santos <hf.santos2332@gmail.com>
2023-09-03 11:09:31 -0300

    Cria rota para obter cursos específicos

---
+++ src/services/coursesService.ts
@@ -0,0 +1,33 @@
+import { courseModel } from "../models";
+
+export const courseService = {
+  findByIdWithEpisodes: async (id: string) => {
+    try {
+      const courseWithEpisodes = await courseModel.findByPk(id, {
+        attributes: [
+          'id', 
+          'name', 
+          'synopsis', 
+          ['thumbnail_url', 'thumbnailUrl']
+        ],
+        include: {
+          association: 'Episodes',
+          attributes: [
+            'id', 
+            'name', 
+            'synopsis', 
+            'order', 
+            ['video_url', 'videoUrl'],
+            ['seconds_long', 'secondsLong']
+          ],
+          order: [['order', 'ASC']],
+          separate: true // Para rodar o include em uma query separada (Necessário para conseguir usar o order do objeto include) 
+        }
+      });
+
+      return courseWithEpisodes;
+    } catch (err) {
+      throw err;
+    }
+  }
+};
